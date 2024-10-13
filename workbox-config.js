10d1200ef5e26d367103eb4a4c5b3acf00a158c4
Eugene Kiselev <e.kiselev@uzum.com>
2024-10-13 17:40:16 +0300

    drop manifest logic

--- workbox-config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  "globDirectory": "site/",
-  "globPatterns": [
-    "**/*.{html,css,ttf,woff,woff2,png,svg,js,ico,json,xml,gz}"
-  ],
-  "swDest": "site/sw.js",
-  "swSrc": "sw-src.js"
-};
